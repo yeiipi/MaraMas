cee8f8f80348175ab52c8306aedb20a08e2ed0ea
Santiago Alvarez Barbosa <santiago.alvarez.barbosa@outlook.com>
2021-10-02 14:16:23 -0500

    doughnut graph implemented with dummy data

--- data/script.js
+++ data/script.js
@@ -5,9 +5,34 @@ const data = {
 };
 
 //Setup data pie chart
-const data1 = {
+const data2 = {
     labels: ["Electronics", "Clothes", "Food"], 
     datasets: [{label: "Number of products", data: [50,23,22], backgroundColor: ["#79cbf8", "#74f0ac", "#f8a36a"], borderWidth:1}],
+};
+
+//Setup data doughnut chart
+const dp = 90
+const datapoints = [dp,(100 - dp)]
+const data3 = {
+    labels: ["Electronics", "Clothes", "Food"], 
+    datasets: [{label: "Number of products", data: datapoints, backgroundColor: ["#0c1c5044", "transparent"], borderColor:["#0077b6", "transparent"], borderWidth:1, cutout: '80%', borderRadius: 10 }],
+};
+
+//------------------------------------------
+
+//counter plugin
+const counter = {
+    id: 'counter',
+    beforeDraw(chart,args,options){
+        const {ctx, chartArea: {top, right, bottom, left, width, height}} = chart;
+        ctx.save();
+        
+        ctx.font = options.fontSize + 'px ' + options.fontFamily;
+        ctx.textAlign = 'centre';
+        ctx.fillStyle = options.fontColor;
+        ctx.fillText(datapoints[0] + "%", width/2.5, (height/2) + options.fontSize * 0.34)
+
+    }
 };
 
 //------------------------------------------
@@ -17,10 +42,11 @@ const config = {
     type: "bar",
     data: data,
 };
+
 //Configurations pie chart
 const config2 = {
     type: 'pie',
-    data: data1,
+    data: data2,
     options: {
       responsive: true,
       plugins: {
@@ -29,11 +55,31 @@ const config2 = {
         },
       }
     },
+  }; 
+
+//Configurations doughnut chart
+const config3 = {
+    type: 'doughnut',
+    data: data3,
+    options: {
+        responsive: true,
+        plugins: {
+          legend: {display: false},
+          counter: {
+              fontColor: 'black', 
+              fontSize: '25',
+              fontFamily: 'Neutrif Pro'
+          }
+        }
+      },
+    plugins: [counter]
   };
 
 //------------------------------------------
+
 //Render bar chart
 const week_ship = new Chart(document.getElementById("week_ship"), config);
 const prod_ship = new Chart(document.getElementById("prod_ship"), config2);
+const progress_ship = new Chart(document.getElementById("progress_ship"), config3);
 
 
