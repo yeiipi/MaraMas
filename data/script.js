83da048a3acda1a3b9815d1f549ebdc5c37d4d99
Santiago Alvarez Barbosa <santiago.alvarez.barbosa@outlook.com>
2021-10-02 15:41:22 -0500

    doughnut graph for ordes on and not on time

--- data/script.js
+++ data/script.js
@@ -1,21 +1,21 @@
 //Setup data bar chart
 const data = {
     labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"], 
-    datasets: [{label: "Shipments", data: [100,69,77,123,35], backgroundColor: ["#0c1c5044"], borderColor:["#0077b6"], borderWidth:1}],
+    datasets: [{label: "Shipments", data: [100,69,77,123,35], backgroundColor: ["#4ADEDE", "#797ef6"], borderColor:["#4ADEDE", "#797ef6"], borderWidth:1}],
 };
 
 //Setup data pie chart
 const data2 = {
     labels: ["Electronics", "Clothes", "Food"], 
-    datasets: [{label: "Number of products", data: [50,23,22], backgroundColor: ["#79cbf8", "#74f0ac", "#f8a36a"], borderWidth:1}],
+    datasets: [{label: "Number of products", data: [50,23,22], backgroundColor: ["#797ef6", "#74f0ac", "#4ADEDE"], borderWidth:1}],
 };
 
-//Setup data doughnut chart
+//Setup data progress chart
 const dp = 90
 const datapoints = [dp,(100 - dp)]
 const data3 = {
     labels: ["Electronics", "Clothes", "Food"], 
-    datasets: [{label: "Number of products", data: datapoints, backgroundColor: ["#0c1c5044", "transparent"], borderColor:["#0077b6", "transparent"], borderWidth:1, cutout: '80%', borderRadius: 10 }],
+    datasets: [{label: "Number of products", data: datapoints, backgroundColor: ["#74f0ac", "transparent"], borderColor:["#74f0ac", "transparent"], borderWidth:1, cutout: '80%', borderRadius: 10 }],
 };
 
 //Setup data line chart
@@ -24,6 +24,13 @@ const data4 = {
     datasets: [{label: "Income", data: [50,23,22,234,12,76,203,53,77,10,123,199], backgroundColor: ['#a6f8b1'], borderColor: ['#61ec73'], borderWidth:1},
     {label: "Income", data: [5,26,12,254,22,16,290,57,27,11,213,19], backgroundColor: ['#ff8f8f'],borderColor: ['#f75959'], borderWidth:1}],
 };
+
+//Setup data dough chart
+const data5 = {
+    labels: ["On Time", "Delayed"], 
+    datasets: [{label: "ETA", data: [1372,197], backgroundColor: ['#797ef6', "#4ADEDE"], borderWidth:1}],
+};
+
 
 //------------------------------------------
 
@@ -64,7 +71,7 @@ const config2 = {
     },
   }; 
 
-//Configurations doughnut chart
+//Configurations progress chart
 const config3 = {
     type: 'doughnut',
     data: data3,
@@ -82,7 +89,7 @@ const config3 = {
     plugins: [counter]
   };
 
-//Configurations doughnut chart
+//Configurations line chart
 const config4 = {
 type: 'line',
    data: data4,
@@ -97,6 +104,21 @@ type: 'line',
    },
  };
 
+//Configurations dough chart
+const config5 = {
+    type: 'doughnut',
+       data: data5,
+       options: {
+         responsive: true,
+         maintainAspectRatio: false,
+         plugins: {
+           legend: {
+             position: 'top',
+           }
+         }
+       },
+     };
+
 //------------------------------------------
 
 //Render bar chart
@@ -104,5 +126,6 @@ const week_ship = new Chart(document.getElementById("week_ship"), config);
 const prod_ship = new Chart(document.getElementById("prod_ship"), config2);
 const progress_ship = new Chart(document.getElementById("progress_ship"), config3);
 const mon_ship = new Chart(document.getElementById("mon_ship"), config4);
+const dough_ship = new Chart(document.getElementById("dough_ship"), config5);
 
 
