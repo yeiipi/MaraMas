77069b77aef4e44a8a439aa97e5bab883e6fc4a9
Santiago Alvarez Barbosa <santiago.alvarez.barbosa@outlook.com>
2021-10-09 15:52:55 -0500

    created gauge meter for fleet efficiency with dummy data

--- data/script.js
+++ data/script.js
@@ -32,6 +32,14 @@ const data5 = {
 };
 
 
+//Setup data gauge meter
+const eff = 90
+const effdata = [(eff*2),(200 - (eff*2))]
+const data6 = {
+  labels : ["Red","Blue"],
+  datasets: [{label: "Gauge", data : effdata, backgroundColor: ["#4ADEDE", '#0077b6'], borderRadius: 5, borderWidth:5, cutout: '70%'}]
+};
+
 //------------------------------------------
 
 //counter plugin
@@ -47,6 +55,20 @@ const counter = {
         ctx.fillText(datapoints[0] + "%", width/2.7, (height/2) + options.fontSize * 0.34)
 
     }
+};
+
+//counter plugin 2 (for gauge meter)
+const counter2 = {
+  id: 'counter2',
+  beforeDraw(chart,args,options){
+      const {ctx, chartArea: {top, right, bottom, left, width, height}} = chart;
+      ctx.save();
+      
+      ctx.font = options.fontSize + 'px ' + options.fontFamily;
+      ctx.textAlign = 'centre';
+      ctx.fillStyle = options.fontColor;
+      ctx.fillText(eff + "%", width/2.7, (height/1.5) + options.fontSize * 0.34)
+  }
 };
 
 //------------------------------------------
@@ -127,9 +149,32 @@ const config5 = {
        },
      };
 
+//Configurations gauge meter
+const config6 = {
+    type:"doughnut",
+    data: data6,
+    options: {
+        responsive: true,
+        circumference: 180,
+        rotation : -90,
+        plugins: {
+          legend: {display: false},
+          counter2: {
+            fontColor: 'black', 
+            fontSize: '25',
+            fontFamily: 'Neutrif Pro'
+        }
+        }
+    },
+    plugins: [counter2]
+}; 
+
+
 //------------------------------------------
 
 //Render bar chart
+const meter_info = new Chart(document.getElementById("meter_info"), config6);
+
 const week_ship = new Chart(document.getElementById("week_ship"), config);
 const prod_ship = new Chart(document.getElementById("prod_ship"), config2);
 const progress_ship = new Chart(document.getElementById("progress_ship"), config3);
