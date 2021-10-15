f664167bd11eaca0d02404ce44d0100998357a61
sadj123 <santiago.d.j@hotmail.com>
2021-10-14 20:29:50 -0500

    git se jodio

---
+++ server/index.js
@@ -0,0 +1,149 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT ||4000
+const cors = require ("cors");
+const { pool } = require("./database");
+const bcrypt= require("bcrypt");
+const { render } = require("ejs");
+const session= require('express-session');
+const flash=  require('express-flash');
+
+// Middleware
+app.use(session({
+    secret: 'secret',
+
+    resave: false,
+
+    saveUninitialized: false,
+
+}));
+app.use(flash());
+app.use(cors());
+app.use(express.json()) //Permite interactuar con req.body
+app.use(express.urlencoded({ extended:false}))
+app.use('/public', express.static('public'));
+
+app.use('/resources', express.static('resources'));
+//Routes
+// Insert conatiner info
+app.post("/container", async(req,res)=>{  //Revisas si usar async o no
+    try{
+        const {id,num,capacity,address} = req.body
+        const newContainer = await pool.query("INSERT INTO container VALUES ($1,$2,$3,$4) RETURNING *",
+        [id,num,capacity,address]
+        );
+        res.json(newContainer.rows[0]);
+        // console.log(num)
+    }catch (err){
+        console.error(err.message);
+    }})  
+
+// Get all container
+//SANTIAGO´S WORK
+app.set('view engine', 'ejs')
+
+app.get("/", (req,res) =>{
+    res.render("home");
+});
+  
+app.get("/users/login", (req,res) =>{
+    res.render("login");
+});
+app.get("/users/register", (req,res) =>{
+    res.render("register");
+});
+
+app.get("/users/dashboard", (req,res) =>{
+    res.render("dashboard", {user: "Sadj"});
+});
+
+app.post("/users/register", async(req ,res)=>{
+    let{ user_name, user_lastname, user_email, user_password}=req.body;
+    console.log({
+        user_name, user_lastname, user_email, user_password
+    }    
+    );
+
+    let errors=[]
+    if(!user_name|| !user_lastname|| !user_email|| !user_password){
+     errors.push({message: "Please enter all the fields"});   
+    }
+    if(user_password.length <6){
+        errors.push({message: "Password is too short"})
+    }
+    if(errors.length>0){
+        res.render('register', {errors})
+    }else{ 
+        let hashpassword= await bcrypt.hash(user_password, 10);
+        console.log(hashpassword);
+    
+    pool.query(`SELECT * FROM users WHERE user_email = $1`, [user_email], (err, results)=> 
+    {
+        if(err){
+            throw err
+            
+        }
+    console.log(results.rows);
+    if(results.rows.length>0){
+        errors.push({message: "Email already registered"});
+        res.render('register', {errors});
+    }
+    else{
+        pool.query(`INSERT INTO users (user_name, user_lastname, user_email, user_password) VALUES ($1, $2, $3, $4)
+        RETURNING user_id, user_password`, [user_name, user_lastname, user_email, hashpassword], (err, results)=>{
+            if(err){
+                throw err
+            }
+            console.log(results);
+            req.flash("succes_msg", "You are now registered, please log in");
+            res.redirect('/users/login');
+        })
+    }
+}
+    )}
+})
+
+
+
+// get specific container
+app.get("/container/:id",async(req,res)=>{
+    try {
+        const{id} = req.params;
+        const container = await pool.query("SELECT * FROM container WHERE id_container = $1",
+        [id])
+        res.json(container.rows[0]);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+//update container
+app.put("/container/:id", async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const {capacity} = req.body;
+        // console.log(capacity)
+        const update_container = await pool.query("UPDATE container SET capacity = $1 WHERE id_container=$2",
+        [capacity,id]);
+        res.json("Container was updated!")
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+// delete container
+app.delete("/container/:id", async(req,res)=>{
+    try {
+        const{id} = req.params;
+        const deleteContainer = await pool.query("DELETE FROM container WHERE id_container=$1",[id]);
+        res.json("Container was deleted")
+    } catch (err) {
+        console.error(err.message)  
+    }
+})
+
+// Ruta prueba 
+app.use("/prueba",require("./routes/prueba"))
+app.listen(PORT, () => {
+    console.log('El servido ha iniciado en puerto '+ PORT);
+});
